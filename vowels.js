4881c81fa08c58a1335207828fe86fb11c8a1f46
sonaliparida9 <sonaliparida805>
2025-08-20 11:38:29 +0530

    count vowels & consonants

--- vowels.js
+++ vowels.js
@@ -1,15 +1,18 @@
 function countVowels(str){
-    const vowels = ["a", "e", "i", "o", "u"];
-    let count = 0;
-
+    const vowels = ["a", "e", "i", "o", "u","A","E","I","O","U"];
+    let vowelCount = 0;
+    let consonantCount = 0;
     // step 1: Iterate through each character in the string
-    for(let char of str.toLowerCase()){
+    for(let char of str){
         // step 2: check if the character is a vowel
         if(vowels.includes(char)){
             // step 3: If true, increment the count
-            count++;
+            vowelCount++;
+        }
+        else{
+            consonantCount++;
         }
     }
-    return count;
+    return {vowels: vowelCount, consonant: consonantCount};
 }
 console.log(countVowels("Hello, world!"));
