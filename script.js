fb771a99f61cac5ee051ae23d6be25e3bfdb3c8a
sonaliparida9 <sonaliparida805>
2025-12-04 21:36:52 +0530

    practice js

--- script.js
+++ script.js
@@ -49,3 +49,43 @@ console.log(b);
 // 12. Create a constant to store the value of PI and log it.
 const pi = Math.PI;
 console.log(pi);
+
+// 13. Reassign a value to a variable declared with let and log the result
+let aa = 12;
+aa = 32;
+console.log(aa);
+
+// 14. Check the type of null and log it.
+var n = null;
+console.log(typeof n);
+// jabhi typeof null nikaala jaayega null ka type humesha object aata hai
+
+// 15. Create a variable with a number as a string(e.g.., "25" and log its type )
+var numstr = "25";
+console.log(typeof numstr);
+
+// 16. use typeof to check the type of a boolean variable.
+var bool = true;
+console.log(typeof bool);
+
+// 17. Declare a variable without initializing it and log its type.
+var uninit;
+console.log(typeof uninit);
+
+// 18. Create a variable to hold a floating-point number and log its value.     
+var floatnum = 12.34;
+console.log(floatnum);
+
+// 19. creat three variables of types string, number and boolean,and log their values.
+let aaa = "sonali", bbb= 23, ccc = false;
+console.log(aaa, bbb, ccc);
+
+// 20. create a value with undefined and log its type.
+var a = undefined;
+console.log(typeof a)
+
+// 21. use const to create an array. Try reassigning the array and observe the error.
+const arr = [1,2,3,4];
+// arr= []; //not reassignable
+arr.pop(); //updatable
+console.log(arr);
