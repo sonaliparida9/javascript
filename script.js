70013834372da61469ced0d3df176cad51d3f434
sonaliparida9 <sonaliparida805>
2025-12-10 17:43:46 +0530

    js practice

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 console.log("sonali", "reading books");
 
 //2. Perform and log the result of 45*2-10
-console.log(45*2-10);
+console.log(45 * 2 - 10);
 
 //3. use console.log() to display the current year
 const dt = new Date();
@@ -16,16 +16,16 @@ let lastName = "Parida";
 console.log(firstName + " " + lastName);
 
 //5. Track the value of a variable by logging it before and after updating
-var a =12;
+var a = 12;
 console.log(a);
-var a = 23
+var a = 23;
 console.log(a);
 
 //6. Use console.error() to simulate an error message.
 console.error("Something want worng");
 
 //7. Log the square of the nuber 12 to the console.
-console.log(12*12);
+console.log(12 * 12);
 
 // 8. Print the type of a variable holding the value true.
 var a = true;
@@ -33,12 +33,12 @@ console.log(typeof a);
 
 // 9. Create a variable holding your age and log whether it's greater than 18.
 var age = 21;
-if(age>18){
-    console.log(true);
+if (age > 18) {
+  console.log(true);
 }
 
 // 10. Log the result of 100/0 and observe the output
-console.log(100/0);
+console.log(100 / 0);
 
 // VAEIABLES AND DATA TYPES
 
@@ -72,125 +72,248 @@ console.log(typeof bool);
 var uninit;
 console.log(typeof uninit);
 
-// 18. Create a variable to hold a floating-point number and log its value.     
+// 18. Create a variable to hold a floating-point number and log its value.
 var floatnum = 12.34;
 console.log(floatnum);
 
 // 19. creat three variables of types string, number and boolean,and log their values.
-let aaa = "sonali", bbb= 23, ccc = false;
+let aaa = "sonali",
+  bbb = 23,
+  ccc = false;
 console.log(aaa, bbb, ccc);
 
 // 20. create a value with undefined and log its type.
 var a = undefined;
-console.log(typeof a)
+console.log(typeof a);
 
 // 21. use const to create an array. Try reassigning the array and observe the error.
-const arr = [1,2,3,4];
+const arr = [1, 2, 3, 4];
 // arr= []; //not reassignable
 arr.pop(); //updatable
 console.log(arr);
 
-
 // Loops
 //22. Write a for loop to print numbers from 1 to 50
-for(let i=1; i<=50; i++){
-    console.log(i);
+for (let i = 1; i <= 50; i++) {
+  console.log(i);
 }
 
 // 23. Use a while loop to sum the numbers from 1 to 100
-let d=1;
-let sum=0;
-while(d<=100){
-    sum = sum + d;
-    d++;
+let d = 1;
+let sum = 0;
+while (d <= 100) {
+  sum = sum + d;
+  d++;
 }
 console.log(sum);
 
 // 24. Create a for...of loop to log each character of the string "javascript".
-var str = "JavaScript"
-for(let character of str){
-    console.log(character);
+var str = "JavaScript";
+for (let character of str) {
+  console.log(character);
 }
 
 // 25. Write a for loop that skips even numbers between 1 and 20.
-for(let i=1; i<=20; i++){
-    if(i%2 !== 0){
-        console.log(i);
-    }
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
 }
 
 // 26.Use a do...while loop to log numbers from 5 to 1.
 var a = 5;
-do{
-    console.log(a);
-    a--;
-}
-while(a>0)
+do {
+  console.log(a);
+  a--;
+} while (a > 0);
 
 // 27. Create a for loop that calculates the factorial of 5.
 var fact = 1;
-for(var i=5; i>0; i--){  
-    fact = fact*i;
+for (var i = 5; i > 0; i--) {
+  fact = fact * i;
 }
 console.log(fact);
 
 //28. Write a nested loop to print a 3*3 grid of numbers.
 var hold = 1;
-for(var i=1; i<4; i++){
-    var str = "";
-    for(var j=1; j<4; j++){
-        str += `${hold} `;
-        hold++
-
-    }
-    console.log(str);
-} 
+for (var i = 1; i < 4; i++) {
+  var str = "";
+  for (var j = 1; j < 4; j++) {
+    str += `${hold} `;
+    hold++;
+  }
+  console.log(str);
+}
 
 //29. Use a for loop to reverse an array [1,2,3,4].
-var array = [1,2,3,4,5]
-for(var i = 0; i < Math.floor(array.length/2); i++){
-    var temp = array[i];
-    array[i] = array[array.length - 1];
-    array[array.length-1] = temp;
+var array = [1, 2, 3, 4, 5];
+for (var i = 0; i < Math.floor(array.length / 2); i++) {
+  var temp = array[i];
+  array[i] = array[array.length - 1];
+  array[array.length - 1] = temp;
 }
 console.log(array);
 
 //30. Use a for...in loop to iterate over an object and log its keys.
-   var obj = {name:"sonali", age:22};
-   for (let key in obj){
-    console.log(key);
-   }
+var obj = { name: "sonali", age: 22 };
+for (let key in obj) {
+  console.log(key);
+}
 //31. Write a while loop that logs numbers from 1 to 100 divisible by 5.
-    var num = 1;
-    while(num<101){
-        if(num % 5 === 0){
-            console.log(num);
-        }
-        num++;
-    }
-
+var num = 1;
+while (num < 101) {
+  if (num % 5 === 0) {
+    console.log(num);
+  }
+  num++;
+}
 
 //  ARRAYS
 // 32. Create an array of your top 5 favorite movies and log it.
- var arr1 = ["apple", "orange", "grapes", "banana", "mango"] ;
- arr1.forEach(function(value){
-    console.log(value);
- })
+var arr1 = ["apple", "orange", "grapes", "banana", "mango"];
+arr1.forEach(function (value) {
+  console.log(value);
+});
 
 // 33. Find and log the second element of an array.
-var arr1 = [1,2,3,4,5];
-console.log(arr[1])
+var arr1 = [1, 2, 3, 4, 5];
+console.log(arr[1]);
 
 // 34.Add two new elements to the start of an array using .unshift().
-var arr1 =[1,2,3,4,5];
-arr1.unshift(0,-1);
+var arr1 = [1, 2, 3, 4, 5];
+arr1.unshift(0, -1);
 console.log(arr1);
 
 // 35. Remove the last elements of an array and log the updated array.
-var arr1 = [1,2,3,4,5];
+var arr1 = [1, 2, 3, 4, 5];
 arr1.pop();
 console.log(arr1);
 
-// 35. Use .slice() to extract the first three elements of an array.
- var arr1 = [1,2,3,4,5];
- console.log(arr1.slice(0,3));
+// 36. Use .slice() to extract the first three elements of an array.
+var arr1 = [1, 2, 3, 4, 5];
+console.log(arr1.slice(0, 3));
+
+// 37. Find the index of a specific element in an array using  .indexof()
+var arr1 = [1, 2, 3, 4, 5];
+console.log(arr1.indexOf(4));
+
+// /38. Check if a value exists in an array using .includes().
+var arr1 = [1, 2, 3, 4, 5];
+console.log(arr1.includes(3));
+
+// 39. Combine two arrays [1,2] and [3,4] using .concat()
+var arr1 = [1, 2];
+var arr2 = [3, 4];
+console.log(arr1.concat(arr2));
+
+//40. Sort an array of numbers[5,2,9,1] in asending order.
+var arr2 = [11, 24, 36, 4, 5];
+for (var j = 0; j < arr2.length - 1; j++) {
+  for (var i = 0; i < arr2.length - j - 1; i++) {
+    if (arr2[i] > arr2[i + 1]) {
+      var temp = arr2[i];
+      arr2[i] = arr2[i + 1];
+      arr2[i + 1] = temp;
+    }
+  }
+}
+console.log(arr2);
+
+// 41. Write a program that creates a copy of an array without mutating/changing the original
+// using spread operator.
+var arr1 = [11, 22, 33, 44];
+var arr2 = [...arr1];
+console.log(arr2);
+
+// using forEach
+var arr1 = [11, 22, 33, 44];
+var arr2 = [];
+
+arr1.forEach(function (value) {
+  arr2.push(value);
+});
+
+arr2.pop(); // changing arr2 to show arr1 is not mutated
+
+console.log(arr1, arr2);
+
+// FUNCTIONS
+// 42. Write a function to check if a number is even or odd.
+function evod(num) {
+  if (num % 2 === 0) return "even";
+  else return "odd";
+}
+console.log(evod(13));
+
+// 43. Create a function to calculate the area of a circle with a given radius.
+function area(r) {
+  return Math.round(Math.PI * r * r);
+}
+console.log(area(2));
+
+// 44. Write a function taht accepts an array and returns the sum of its element.
+function sumofarr(arr) {
+  var sum = 0;
+  arr.forEach(function (v) {
+    sum = sum + v;
+  });
+  return sum;
+}
+console.log(sumofarr([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// 45. Create a function that checks if a string starts with a specific character.
+function cheker(str, char) {
+  return str.toLowerCase().startsWith(char.toLowerCase());
+}
+console.log(cheker("Sona", "s"));
+
+// 46. Write a function to find the maximum of two numbers.
+function max(a, b) {
+  if (a > b) return a;
+  else if (a < b) return b;
+  else return "equals";
+}
+console.log(max(1, 1));
+
+// 47. Create a function that take a number and retuns its factorial
+function getFact(num) {
+  var fact = 1;
+  for (var i = 1; i <= num; i++) {
+    fact = fact * i;
+  }
+  return fact;
+}
+console.log(getFact(5));
+
+// 48. Write a function taht accepts a string and returns its reverse.
+function revofstr(str){
+    return str.split("").reverse().join("");
+}
+console.log(revofstr("sonali"));
+
+// 49. Create a function to find the largest number in an array.
+function abcd(arr){
+    var max = 0;
+    for(var i=1; i<arr.length; i++){
+        if(arr[i]>arr[max]){
+            max = i;
+        }
+    }
+    return arr[max];
+}
+console.log(abcd([1,2,3,34,55,5,7,45,90]));
+
+// 50. Write a function that convert string to kebab-case.
+function kebab(str){
+    // return str.split(" ").join('-');
+    return str.replaceAll(' ','-')
+}
+console.log(kebab("hey kaise ho"));
+
+// 51. create a function thats log "hello world!" every time it is calls
+function abcd(){
+    console.log("Hello World!");
+}
+abcd()
+abcd()
+abcd()
